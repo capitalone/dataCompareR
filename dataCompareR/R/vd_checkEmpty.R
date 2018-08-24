5e5d70dcf127e538f6302ba70cabff70c946be27
rob <rnobleeddy@gmail.com>
2018-08-24 10:42:41 +0100

    - Split out the dataframe NA checks from checking empty
    - Modified test to allow data frames with no rows but with columns to pass validation. Column-less data frames still fail.
    
    - Added functions to check for NA
    
    - Unit tested new functions

--- dataCompareR/R/vd_checkEmpty.R
+++ dataCompareR/R/vd_checkEmpty.R
@@ -13,6 +13,8 @@
 
 #' checkEmpty
 #'
+#' Checks if a df is actually a single NA, or has no columns
+#'
 #' @param df a data frame
 #' @return None. Stops if empty.
 #' @examples
@@ -24,12 +26,39 @@ checkEmpty <- function(df) {
   # So this logic ensures we proceed for large data frames without running the is.na
   # step
   
-  
-  if(nrow(df)>1) {
-    return()
-  } else if (nrow(df) ==0 || is.na(df)) {
+  if(is.null(ncol(df)) || is.na(ncol(df)) || ncol(df)==0) {
+    stop("ERROR : One oe more dataframes have no coumns")
+  }
+}
+
+
+
+#' CheckNA 
+#' 
+#' Checks a data frame is NA - if so, stops
+#' 
+#' @param df A (probable) dataframe
+#' 
+#' @return Nothing. Errors is df is NA
+
+checkNA <- function(df) {
+  if(isSingleNA(df)) {
     stop('ERROR : One or more dataframes are empty')
   }
 }
 
 
+#' isSingleNA
+#' 
+#' Boolean function - T if x is a single NA. False otherwise.
+#' 
+#' @param x literally anything
+#' 
+#' @returns boolean
+isSingleNA <- function(x) {
+  if(is.vector(x) && length(x) == 1 && is.na(x)) {
+    TRUE
+  } else {
+    FALSE
+  }
+}
