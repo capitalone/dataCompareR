308d4730d9779738d4be073dd21e98817f74ca59
Rob Noble-Eddy <rnobleeddy@gmail.com>
2018-08-31 11:08:17 +0100

    Save report fix - allows users to specify number of mismatch rows (#55)
    * Fixes the problem that the user cannot configure the number of mismatches in the report created by saveReport.
    
    Add ellipses so the user can pass `mismatchCount` to control the number of mismatches printed. Also added `printAll` to allow user to print all.
    
    * Updated roxygen comments
    
    * Update documentation

--- dataCompareR/tests/testthat/testSaveReport.R
+++ dataCompareR/tests/testthat/testSaveReport.R
@@ -35,6 +35,24 @@ test_that("checks save report works", {
   
   aaa <- rCompare(iris,iris2)
   
+  tmpDir <- tempdir()
+  
+  # Test some basic arguments
+  expect_message(saveReport(compareObject = aaa, reportName = 'testing',reportLocation = tmpDir, HTMLReport = TRUE), regexp = "Using default stylesheet")
+  expect_message(saveReport(aaa, 'testing2',reportLocation = tmpDir, HTMLReport = FALSE), regexp = "Using default stylesheet")
+  expect_message(saveReport(aaa, 'testing3',reportLocation = tmpDir, HTMLReport = FALSE, showInViewer = FALSE), regexp = "Using default stylesheet")
+  
+  # Check the files exist
+  expect_true(file.exists(file.path(tmpDir, "testing.Rmd")))
+  expect_true(file.exists(file.path(tmpDir, "testing.md")))
+  expect_true(file.exists(file.path(tmpDir, "testing.html")))
+  expect_true(file.exists(file.path(tmpDir, "testing2.Rmd")))
+  expect_true(file.exists(file.path(tmpDir, "testing3.Rmd")))
+  expect_true(!file.exists(file.path(tmpDir, "testing2.md")))
+  expect_true(!file.exists(file.path(tmpDir, "testing3.md")))
+  expect_true(!file.exists(file.path(tmpDir, "testing2.html")))
+  expect_true(!file.exists(file.path(tmpDir, "testing3.html")))
+  
   # Check no errors
   expect_error(saveReport(aaa, 'testing',reportLocation = '.', HTMLReport = TRUE), NA)
   expect_error(saveReport(aaa, 'testing', reportLocation = '.',HTMLReport = FALSE), NA)
@@ -65,5 +83,61 @@ test_that("checks save report argument validation works", {
   expect_error(saveReport(aaa, reportName = 'random',HTMLReport = 'YesPlease'))
   expect_error(saveReport(aaa, reportName = 'random',showInViewer = 'YesPlease'))
   expect_error(saveReport(aaa, reportName = 'random',stylesheet = 'Please fetch it from my favourite website'))
+  expect_error(saveReport(aaa, reportName = 'random',printAll = iris))
+  
+})
+
+
+
+test_that("checks save report passing extra arguments works", {
+  
+  skip_on_cran()
+  
+  # There's very little we can do here - just check it runs with no errors
+  iris2 <- iris
+  iris2 <- iris2[1:130,]
+  iris2$Sepal.Length <- iris2$Sepal.Length + 1
+  
+  
+  aaa <- rCompare(iris,iris2)
+  
+  tmpDir <- tempdir()
+  
+  # Test some basic arguments
+  expect_message(saveReport(aaa, 'testing',reportLocation = tmpDir, HTMLReport = TRUE), regexp = "Using default stylesheet")
+  
+  expect_message(saveReport(aaa, 'testing-big',reportLocation = tmpDir, 
+                            HTMLReport = TRUE, mismatchCount = 10), 
+                 regexp = "Using default stylesheet")
+  
+  expect_message(saveReport(aaa, 'testing-all',reportLocation = tmpDir, 
+                            HTMLReport = TRUE, printAll = TRUE),
+                 regexp = "Using default stylesheet")
+                 
+  expect_true(file.exists(file.path(tmpDir, "testing.Rmd")))
+  expect_true(file.exists(file.path(tmpDir, "testing.md")))
+  expect_true(file.exists(file.path(tmpDir, "testing.html")))
+  
+  expect_true(file.exists(file.path(tmpDir, "testing-big.Rmd")))
+  expect_true(file.exists(file.path(tmpDir, "testing-big.md")))
+  expect_true(file.exists(file.path(tmpDir, "testing-big.html")))
+  
+  expect_true(file.exists(file.path(tmpDir, "testing-all.Rmd")))
+  expect_true(file.exists(file.path(tmpDir, "testing-all.md")))
+  expect_true(file.exists(file.path(tmpDir, "testing-all.html")))
+  
+  # Read the files so we can check they're correct
+  file1 <- readLines(file.path(tmpDir, "testing.md"))
+  file2 <- readLines(file.path(tmpDir, "testing-big.md"))
+  file3 <- readLines(file.path(tmpDir, "testing-all.md"))
+  
+  expect_equal(file1[66],"Showing sample of size 5")
+  expect_equal(file2[66],"Showing sample of size 10")
+  expect_equal(file3[66],"Showing sample of size 130")
+  
+  # check lengths differ by 5 betwee 1 and 2 
+  expect_equal(length(file1)+5, length(file2))
+  # and 125 from 1 to 3
+  expect_equal(length(file1)+125, length(file3))
   
 })
